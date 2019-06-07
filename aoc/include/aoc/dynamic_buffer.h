a60523d24be04da2d497bbd1e37654a33887fc84
Nicole Mazzuca <mazzucan@outlook.com>
2019-06-07 10:49:39 -0700

    add stretchy buffers

---
+++ aoc/include/aoc/dynamic_buffer.h
@@ -0,0 +1,55 @@
+#ifndef AOC_DYNAMIC_BUFFER_H
+#define AOC_DYNAMIC_BUFFER_H
+
+#include <aoc/utility.h>
+
+size_t _Aoc_dynamic_buffer_length(void const* self);
+size_t _Aoc_dynamic_buffer_capacity(void const* self);
+
+void _Aoc_dynamic_buffer_set_length(void* self, size_t length);
+void _Aoc_dynamic_buffer_increment_length(void* self);
+void _Aoc_dynamic_buffer_decrement_length(void* self);
+
+void* _Aoc_dynamic_buffer_with_capacity(size_t el_size, size_t capacity);
+
+void* _Aoc_dynamic_buffer_resize(
+  void* self,
+  size_t el_size,
+  size_t new_cap
+);
+void* _Aoc_dynamic_buffer_resize_if_at_capacity(
+  void* self,
+  size_t el_size
+);
+
+void _Aoc_dynamic_buffer_free(void* self);
+
+#define db_length(self) _Aoc_dynamic_buffer_length(self)
+#define db_capacity(self) _Aoc_dynamic_buffer_capacity(self)
+
+#define db_start(self) (&(self)[0])
+#define db_end(self) (&(self)[db_length(self)])
+
+#define db_new(type) \
+  _Aoc_dynamic_buffer_with_capacity(sizeof(type), 8)
+#define db_new_with_capacity(type, capacity) \
+  _Aoc_dynamic_buffer_with_capacity(sizeof(type), capacity)
+#define db_free(self) _Aoc_dynamic_buffer_free(self)
+
+#define db_resize(self, cap) \
+  ((self) = _Aoc_dynamic_buffer_resize(self, sizeof(*self), cap))
+
+#define db_push(self, item) \
+  ( \
+    ((self) = _Aoc_dynamic_buffer_resize_if_at_capacity(self, sizeof(*self))), \
+    ((self)[db_length(self)] = (item)), \
+    _Aoc_dynamic_buffer_increment_length(self) \
+  )
+
+#define db_pop(self) \
+  (_Aoc_decrement_length(self), (self)[db_length(self)])
+
+#define db_set_length(self, length) \
+  _Aoc_dynamic_buffer_set_length(self, length)
+
+#endif // AOC_DYNAMIC_BUFFER_H
--- day-1b/main.c
+++ day-1b/main.c
@@ -1,3 +1,4 @@
+#include <aoc/dynamic_buffer.h>
 #include <aoc/string.h>
 #include <aoc/read.h>
 
@@ -9,9 +10,8 @@ void do_the_thing(string const data) {
   int acc = 0;
   char buffer[32] = {0};
 
-  size_t capacity = 4096;
-  int* freq_buffer = calloc(capacity, sizeof(*freq_buffer));
-  int* freq_buffer_end = freq_buffer + 1;
+  int* freq_buffer = db_new_with_capacity(int, 8);
+  db_push(freq_buffer, 0);
 
   string rest_of_lines = data;
 
@@ -19,13 +19,17 @@ void do_the_thing(string const data) {
     string const line = get_line(rest_of_lines);
     size_t const length = string_length(line);
 
+    if (line.end == data.end) {
+      // start over from the beginning
+      rest_of_lines.start = data.start;
+    } else {
+      rest_of_lines.start = line.end + 1;
+    }
+
     if (length == 0) {
-      if (line.end == data.end) {
-        // start over from the beginning
-        rest_of_lines.start = data.start;
-      }
       continue;
     }
+
     if (length > 31) {
       goto invalid_format;
     }
@@ -41,35 +45,33 @@ void do_the_thing(string const data) {
       acc += tmp;
     }
 
-    rest_of_lines.start = line.end + 1;
-
-    for (int const* it = freq_buffer; it != freq_buffer_end; ++it) {
-      if (acc == *it) {
-        printf("Answer is %d\n", acc);
-        return;
+    {
+      int const* it;
+      int const* end = db_end(freq_buffer);
+      for (it = freq_buffer; it != end; ++it) {
+        if (acc == *it) {
+          printf("Answer is %d\n", acc);
+          goto success;
+        }
       }
     }
 
-    if (freq_buffer_end == freq_buffer + capacity) {
-      int* tmp = calloc(capacity * 2, sizeof(*freq_buffer));
-      memcpy(tmp, freq_buffer, capacity * sizeof(*freq_buffer));
-      freq_buffer = tmp;
-      freq_buffer_end = freq_buffer + capacity;
-      capacity *= 2;
-    }
-
-    *freq_buffer_end++ = acc;
+    db_push(freq_buffer, acc);
 
     continue;
   invalid_format:
     fprintf(stderr, "Invalid format: %.*s", (int)length, line.start);
     return;
   }
+
+success:
+  db_free(freq_buffer);
 }
 
 int main() {
   string data = aoc_read(1, S("input.txt"));
   do_the_thing(data);
+  free(data.start);
 
   return 0;
 }
